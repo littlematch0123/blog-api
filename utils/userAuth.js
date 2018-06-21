f4e13250a1f70f3a44313e63ee2c71c25614bec2
xiaohuochai <121631835@qq.com>
2018-06-21 16:03:44 +0800

    测试帐号不可进行用户端操作

--- utils/userAuth.js
+++ utils/userAuth.js
@@ -5,14 +5,17 @@ const { secret } = jwtConfig
 const adminAuth = (req, res, next) => {
   const token = req.headers.authorization
   if (token) {
-    jwt.verify(token, secret, err => {
+    jwt.verify(token, secret, (err, decoded) => {
       if (err) {
         if (err.name === 'TokenExpiredError') {
           return res.status(401).json({ code: 3, message: '认证码失效，请重新登录' })
         }
         return res.status(401).json({ code: 3, message: '认证失败' })
       }
-      next()
+      if (decoded.test === true) {
+        return res.status(403).json({ code: 4, message: '测试帐号只允许查看，不允许修改' })
+      }
+      next()      
     })
   } else {
     return res.status(401).json({ code: 3, message: '请提供认证码' })
