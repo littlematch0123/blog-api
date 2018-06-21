7ffaf9cd167e7cb77e6e4a9548c926c6e3cd0f8d
xiaohuochai <121631835@qq.com>
2018-06-21 13:55:50 +0800

    用户列表倒序排列

--- apis/user.js
+++ apis/user.js
@@ -8,7 +8,7 @@ module.exports = app => {
   app.get('/users/:id', (req, res) => {
     User.findById(req.params.id)
       .populate({ path: 'comments', select: 'createdAt post' })
-      .populate({ path: 'likes', select: 'createdAt post' }).select('username').sort('-createdAt').exec((err, doc) => {
+      .populate({ path: 'likes', select: 'createdAt post' }).select('username').exec((err, doc) => {
         if (err) return res.status(500).json({ code: 2, message: err.message, err })
         if (doc === null) return res.status(404).json({ code: 1, message: '用户不存在，请刷新后再试' })
         return res.status(200).json({ code: 0, message: '获取用户信息成功', result: { doc } })
@@ -35,7 +35,7 @@ module.exports = app => {
 
   /* 加载所有用户 */
   app.get('/users', (req, res) => {
-    User.find().select('username status likes comments').exec((err, docs) => {
+    User.find().select('username status likes comments').sort('-createdAt').exec((err, docs) => {
       if (err) return res.status(500).json({ code: 2, message: err.message, err })
       return res.status(200).json({ code: 0, message: '获取用户成功', result: { docs } })
     })
