91e63cfa5a480d5a3dd6796cba3f75da6164eb8c
xiaohuochai <121631835@qq.com>
2018-06-21 13:09:22 +0800

    为评论增加管理员是否已读的属性

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,7 @@ const CommentSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User', index: true },
     post: { type: Schema.Types.ObjectId, ref: 'Post', index: true },
+    viewed: { type: Boolean, default: false },
     content: { type: String, required: true, validate: { validator: v => v.trim().length, message: '内容不能为空' } }
   },
   { timestamps: true }
